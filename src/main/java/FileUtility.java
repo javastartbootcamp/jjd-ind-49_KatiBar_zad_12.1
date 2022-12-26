fef1bfdad0de62bdac9cb011fbf7afded0586190
Katarzyna Baranska <katarzyna.kinga.baranska@gmail.com>
2022-12-26 13:49:55 +0100

    Dodanie klasy File Utility, w której znajdują się metody do odczytu, przetwarzania i zapisu pliku. + Logika testowa

---
+++ src/main/java/FileUtility.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FileUtility {
+    private double[] results;
+    private String[] wordsInLine;
+
+    public void readFile(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        int lines = countLines(fileName);
+        wordsInLine = new String[lines];
+        for (int i = 0; i < lines; i++) {
+            wordsInLine[i] = scanner.nextLine();
+        }
+    }
+
+    public void printResults() {
+        results = new double[wordsInLine.length];
+        for (int i = 0; i < wordsInLine.length; i++) {
+            String[] split = wordsInLine[i].split(" ");
+            double number1 = Double.parseDouble(split[0]);
+            String option = split[1];
+            double number2 = Double.parseDouble(split[2]);
+            double result = calculateResult(number1, option, number2);
+            results[i] = result;
+            System.out.println(number1 + " " + option + " " + number2 + " = " + result);
+        }
+    }
+
+    public void saveResults(String fileName) throws IOException {
+        try (
+                var fileWriter = new FileWriter(fileName);
+                var bufferedWriter = new BufferedWriter(fileWriter);
+                ){
+            for (int i = 0; i < results.length; i++) {
+                bufferedWriter.write(wordsInLine[i] + " = " + results[i]);
+                bufferedWriter.newLine();
+            }
+        }
+    }
+
+    private static double calculateResult(double number1, String option, double number2) {
+        double result = 0;
+        switch (option) {
+            case "+":
+                result = number1 + number2;
+                break;
+            case "-":
+                result = number1 - number2;
+                break;
+            case "*":
+                result = number1 * number2;
+                break;
+            case "/":
+                result = number1 / number2;
+                break;
+        }
+        return result;
+    }
+
+    private static int countLines(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        int lines = 0;
+        while (scanner.hasNextLine()) {
+            lines++;
+            scanner.nextLine();
+        }
+        return lines;
+    }
+}
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 class Main {
     public static void main(String[] args) {
-        // uzupełnij rozwiązanie
+        String fileName = "operations.txt";
+        FileUtility fileUtility = new FileUtility();
+        try {
+            fileUtility.readFile(fileName);
+            fileUtility.printResults();
+            fileUtility.saveResults(fileName);
+        } catch (FileNotFoundException e) {
+            System.err.println("Brak pliku o nazwie: " + fileName);;
+        } catch (IOException e) {
+            System.err.println("Nie udało się dopisać nowych danych do pliku: " + fileName);
+        }
     }
 }
